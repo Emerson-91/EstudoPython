fd2c450e2301fc817cbf1d1ef751ddaf6ac00889
emerson-91 <emerson.hcdossantos@gmail.com>
2020-05-08 18:11:06 -0400

    Exercicio 81 Curso em video

--- exerc_81.py
+++ exerc_81.py
@@ -4,9 +4,14 @@ B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e esta ou nao na lista """
 
 lista = []
-#for c in len(lista):
-n = int(input("Digite um valor: "))
-lista.append(n)
-r = str(input("Quer continuar[S/N]: ")).upper()[0]
-    while r in "N":
+while True:
+    n = int(input("Digite um valor: "))
+    lista.append(n)
+    r = str(input("Quer continuar[S/N]: ")).upper()[0]
+    if r in "N":
         break
+#for c in len(lista):
+lista.sort(reverse=True)
+print(f"Foram digitados {len(lista)} numeros")
+print(f'lista em ordem decrescente: {lista}')
+
